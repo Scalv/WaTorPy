76edfbbdc9fcc847d5d256834d11b0dd00f28ff8
Sam Calvin <Scalvin11@gmail.com>
2018-09-09 21:04:39 -0400

    began implementing tkinter gui

--- wator.py
+++ wator.py
@@ -1,4 +1,7 @@
 import random
+import tkinter
+from tkinter import ttk
+
 from animals import Fish, Shark, AnimalFactory
 from map import Map
 import constants
@@ -6,11 +9,19 @@ from turn_handler import TurnHandler
 
 
 if __name__ == "__main__":
-    map = Map(constants.MAP_HEIGHT, constants.MAP_WIDTH)
+    # map = Map(constants.MAP_HEIGHT, constants.MAP_WIDTH)
+    #
+    # factory = AnimalFactory(constants.NUM_FISH, constants.NUM_SHARKS, map)
+    # factory.spawn_fish()
+    #
+    # turns = TurnHandler(map)
+    # for i in range(21):
+    #     turns.step()
 
-    factory = AnimalFactory(constants.NUM_FISH, constants.NUM_SHARKS, map)
-    factory.spawn_fish()
+    root = Tk()
+    root.title("Config")
 
-    turns = TurnHandler(map)
-    for i in range(21):
-        turns.step()
+    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    mainframe.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
