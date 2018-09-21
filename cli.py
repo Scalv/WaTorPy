f4069b64862b81549ece8b33e44f758d308b13ef
Sam Calvin <Scalvin11@gmail.com>
2018-09-20 21:18:07 -0400

    cli progress

--- cli.py
+++ cli.py
@@ -14,41 +14,60 @@ class CLI():
         self.auto_step_setup = False
 
         self.turns = None
+        self.map = None
 
     def welcome(self):
         print(LOGO)
         self.print_constants()
         self.menu()
 
-    def start(self):
-        # TODO autostep (time per step), step on enter pressed
-        map = Map(constants["MAP_HEIGHT"], constants["MAP_WIDTH"])
-        factory = AnimalFactory(constants["NUM_FISH"], constants["NUM_SHARKS"], map)
+    def init_game(self):
+        self.map = Map(constants["MAP_HEIGHT"], constants["MAP_WIDTH"])
+        factory = AnimalFactory(constants["NUM_FISH"], constants["NUM_SHARKS"],
+                                self.map)
         factory.spawn_fish()
-        turns = TurnHandler(map)
-        self.turns = turns
+        self.turns = TurnHandler(self.map)
 
+        self.game_loop()
+
+    def game_loop(self):
         while True:
-            choice = self.step_menu()
-            choice_dict = {"s" : turns.step, "f" : map.print_fish,
-                           "h" : map.print_sharks, "q" : self.menu,
+            choice = self.step_general_menu()
+            choice_dict = {"s" : self.turns.step, "f" : self.map.print_fish,
+                           "h" : self.map.print_sharks, "q" : self.menu,
                            "a" : self.begin_auto_step, "u" : self.setup_auto_step,
-                           }
-            # better way to handle this
-            if choice == "c":
-                self.clear_screen()
-                turns.step()
-            else:
-                choice_dict[choice]()
+                           "c" : self.clear_and_step,}
 
-    def step_menu(self):
-        print("(A)uto Step | (S)tep | (C)lear Console and Step | (F)ish List | S(h)ark List | A(u)to Step Setup | (Q)uit |")
+            choice_dict[choice]()
+
+    def step_general_menu(self):
+        print("(S)tep | (C)onfig and Info | (Q)uit |")
         choice = input("")
-        if choice.lower() in "aucsfhq":
+        if choice.lower() == "s":
+            return self.step_type_menu()
+        elif choice.lower() == "c":
+            return self.step_config_and_info_menu()
+        elif choice.lower() == "q":
             return choice.lower()
         else:
             # print something to correct them
             self.step_menu()
+
+    def step_type_menu(self):
+        print("Step Choices:\n (S)tep\n (A)uto Step\n (C)lear and Step")
+        choice = input("")
+        if choice.lower() in "sac":
+            return choice
+        else:
+            self.step_type_menu()
+
+    def step_config_and_info_menu(self):
+        print("Config Choices:\n A(u)to Step Setup\n (F)ish List \n S(h)ark list")
+        choice = input("")
+        if choice.lower() in "ufh":
+            return choice
+        else:
+            self.step_config_and_info_menu()
 
     def begin_auto_step(self):
         if self.auto_step_setup:
@@ -56,17 +75,17 @@ class CLI():
                 self.clear_screen()
                 self.turns.step()
                 time.sleep(self.auto_step_time_between_steps)
+        else:
+            print("Set up autostep in config")
 
     def setup_auto_step(self):
         self.auto_step_num_steps = int(input("Number of Steps: "))
         self.auto_step_time_between_steps = float(input("Time Between Steps (seconds): "))
         self.auto_step_setup = True
 
-    def clear_screen(self):
-        if os.name == 'nt':
-            _ = os.system('cls')
-        else:
-            _ = os.system('clear')
+    def clear_and_step(self):
+        self.clear_screen()
+        self.turns.step()
 
 
     def change_constants(self):
@@ -85,10 +104,6 @@ class CLI():
             self.change_constants()
         self.menu()
 
-    def default_constants(self):
-        for k, v in defaults.items():
-            constants[k] = v
-
     def get_constants(self):
         for name, value in constants.items():
             print("{}: Current: {} New: ".format(name.title().replace("_", " "), value), end="")
@@ -103,7 +118,7 @@ class CLI():
         pass
 
     def menu(self):
-        switch = {"1" : self.start, "2" : self.change_constants,
+        switch = {"1" : self.init_game, "2" : self.change_constants,
                   "3" : self.description, "4" : self.quit}
         self.print_menu()
         while True:
@@ -121,6 +136,16 @@ class CLI():
     def print_constants(self):
         for k, v in PRINT_FRIENDLY.items():
             print(k.format(v))
+
+    def default_constants(self):
+        for k, v in defaults.items():
+            constants[k] = v
+
+    def clear_screen(self):
+        if os.name == 'nt':
+            _ = os.system('cls')
+        else:
+            _ = os.system('clear')
 
     def quit(self):
         sys.exit(0)
