1a78dbfb692b5c15d22fa80d2868f63a1a263bc3
Sam Calvin <Scalvin11@gmail.com>
2018-09-25 20:29:52 -0400

    Documentation

--- map.py
+++ map.py
@@ -1,3 +1,15 @@
+"""
+    Map contains the simulations map, and methods affecting and returning
+    information about the map.
+
+    - The map itself is a 2d array/list, where the first index is essentially
+    the y coordinate, and the second is the x. All methods accept coordinates
+    as [x, y] and interpret the map as [y][x] to avoid complications outside
+    the class.
+
+    - Contains all animal references in the program.
+"""
+
 from settings import constants
 
 class Map():
@@ -10,6 +22,9 @@ class Map():
         self.fish = []
         self.sharks = []
 
+    # generate_map and clear_map both initialize the map to a 2d array/List
+    # of height/width provided on initializisation of class.
+    # TODO merge
     def generate_map(self):
         for i in range(self.height):
             self.map.append([0 for x in range(self.width)])
@@ -18,6 +33,8 @@ class Map():
         for i in range(len(self.map)):
             self.map[i] = [0 for x in range(self.width)]
 
+    # Prints the map to cli with information about fish and sharks
+    # TODO rename
     def print_formatted_map(self):
         print("-" * (constants["MAP_WIDTH"] + 2))
         print("Fish Alive: {}".format(len(self.fish)))
@@ -25,6 +42,7 @@ class Map():
         self.print_map()
         print("-" * (constants["MAP_WIDTH"] + 2))
 
+    # Prints the map with axis's numbered
     def print_map(self):
         print("  ", end="")
         for i in range(constants["MAP_WIDTH"]):
@@ -41,12 +59,15 @@ class Map():
                     print(j, end="")
             print("\n")
 
+    # Takes a list of animals and prints a string representation of each
     def print_animals(self, animal_list):
         i = 0
         for a in animal_list:
             i += 1
             print("{} {}".format(i, a.formatted_string()))
 
+    # print_fish and print_shark utilize print_animals to print a list of
+    # each animal
     def print_fish(self):
         print("Fish Alive:")
         self.print_animals(self.fish)
@@ -55,6 +76,7 @@ class Map():
         print("Sharks Alive:")
         self.print_animals(self.sharks)
 
+    # add and remove fish and sharks from the map
     def add_fish(self, fish):
         self.fish.append(fish)
 
@@ -64,28 +86,35 @@ class Map():
     def remove_shark(self, shark):
         self.sharks.remove(shark)
 
+    # returns true if the space passed is empty
     def check_space_empty(self, coords):
         if self.map[coords[1]][coords[0]] == 0:
             return True
         return False
 
+    # returns true if the space has the icon of a certain animal
     def check_space_has_icon(self, coords, icon):
         if self.map[coords[1]][coords[0]] == icon:
             return True
         return False
 
+    # removes the fish at coordinates provided from the list of fish
     def kill_fish_at_coords(self, coords):
         for f in self.fish:
             if f.coords == coords:
                 self.fish.remove(f)
 
+    # iterates over a list and changes the coordinate in the map to the
+    # provided icon
     def draw_from_list(self, list, icon):
         for a in list:
             c = a.coords
             self.map[c[1]][c[0]] = icon
 
+    # clears map and redraws all animals, called each time an animal moves
+    # so the next to move has an accurate map
     def update_map(self):
-        # inefficient to do each time an animal moves, new function with animal instance passed?
+        # TODO inefficient to do each time an animal moves, new function with animal instance passed?
         self.clear_map()
         self.draw_from_list(self.fish, constants["FISH_ICON"])
         self.draw_from_list(self.sharks, constants["SHARK_ICON"])
