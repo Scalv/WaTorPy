48fa46950336810f4d65381a527ce10044eb0171
Sam Calvin <Scalvin11@gmail.com>
2018-10-06 21:18:39 -0400

    Documentation

--- turn_handler.py
+++ turn_handler.py
@@ -1,3 +1,8 @@
+"""
+    TurnHandler tracks and processes each step
+    It checks each fish and shark's move each step, confirms the move as valid
+    and tells the animal that the move is valid.
+"""
 from map import Map
 from animals import Fish, Shark
 from settings import constants
@@ -8,6 +13,8 @@ class TurnHandler():
     def __init__(self, map):
         self.map = map
 
+    # Called each step, everything is handled internally besides printing the
+    # map
     def step(self):
         self.steps += 1
         print("Step: {}".format(self.steps))
@@ -15,7 +22,8 @@ class TurnHandler():
         self._fish_step()
         self.map.print_formatted_map()
 
-
+    # Loops over all sharks, ages them, makes sure their alive then has them
+    # move.
     def _shark_step(self):
         for shark in self.map.sharks:
             shark.step_age()
@@ -28,6 +36,8 @@ class TurnHandler():
 
             self._shark_move(shark, invalid_moves, surrounding_fish)
 
+    # checks for fish in tiles surrounding a shark, returns which coordinates
+    # contain a fish
     def _check_surrounding_shark(self, shark):
         fish = []
         for i in shark.surrounding_tiles:
@@ -35,6 +45,9 @@ class TurnHandler():
                 fish.append(i)
         return fish
 
+    # Asks a shark for a valid move and confirms the move.
+    # If there are fish in surrounding tiles, the shark chooses one of those moves
+    # otherwise it moves randomly, or does not move if there are no valid moves.
     def _shark_move(self, shark, invalid, surrounding_fish):
         if len(surrounding_fish) > 0:
             move = shark.move_to_fish(surrounding_fish)
@@ -51,6 +64,7 @@ class TurnHandler():
                     break
         self.map.update_map()
 
+    # Loops over all fish, ages each, checks if each can breed them has each move
     def _fish_step(self):
         new_fish = []
         for fish in self.map.fish:
@@ -62,6 +76,7 @@ class TurnHandler():
         for fish in new_fish:
             self.map.add_fish(fish)
 
+    # Gets and confirms a move from a fish, if it has no valid moves it does not move.
     def _fish_move(self, fish):
         c = fish.move(self._get_invalid_moves_fish(fish))
         if c is None:
@@ -70,6 +85,8 @@ class TurnHandler():
             fish.confirm_move()
         self.map.update_map()
 
+    # checks the tiles surrounding a fish and returns moves which would contain
+    # other animals
     def _get_invalid_moves_fish(self, animal):
         invalid_moves = []
         for i in animal.surrounding_tiles:
@@ -79,6 +96,8 @@ class TurnHandler():
                 invalid_moves.append(invalid_coord)
         return invalid_moves
 
+    # Checks the tiles surrounding a shark and returns moves which would contain
+    # other sharks, but not other fish.
     def _get_invalid_moves_shark(self, animal):
         invalid_moves = []
         for i in animal.surrounding_tiles:
